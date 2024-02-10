ea8f48c13562ef6586de1eec08f9d717bd6db0aa
OnkarEnterprise <156690332+OnkarEnterprise@users.noreply.github.com>
2024-02-10 07:49:08 +0530

    change ext (#1)
    * fix3
    
    * Rename 0.JPG to 0.jpg
    
    * Rename 1.JPG to 1.jpg
    
    * Rename 2.JPG to 2.jpg
    
    * Rename 3.JPG to 3.jpg
    
    * Rename 4.JPG to 4.jpg
    
    * Rename 5.JPG to 5.jpg
    
    ---------
    
    Co-authored-by: Atique Khan <atique.khan@zeza.tech>

--- onkar/src/assets/index.js
+++ onkar/src/assets/index.js
@@ -8,7 +8,7 @@ import close from "./close.svg";
 import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
-import twitter from "./twitter.svg";
+
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
@@ -42,7 +42,7 @@ export {
   facebook,
   instagram,
   linkedin,
-  twitter,
+
   people01,
   people02,
   people03,
